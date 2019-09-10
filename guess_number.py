367167bc915d029e62de26cf1bc806066d0567f9
lyubomyr83 <lyubomyr83@mail.ru>
2019-09-10 19:03:36 +0300

    1

---
+++ guess_number.py
@@ -0,0 +1,32 @@
+# Игра "Угадай число"
+import random
+
+print('Привет! Как тебя зовут?')
+name = input()
+print('Что ж, ' + name + ', я загадываю число от 1 до 20.')
+
+number = random.randint(1, 20)
+
+counter = 0
+for counter in range(6):
+    if counter == 0:
+        print('Попробуй угадать:')
+    else:
+        print('Попробуй угадать снова:')
+
+    guess_number = int(input()) #Загаданное число
+
+    if guess_number > number:
+        print('Твое число слишком большое.')
+    if guess_number < number:
+        print('Твое число слишком маленькое.')
+    if guess_number == number:
+        break
+
+if guess_number == number:
+    counter = str(counter+1)
+    print('Отлично, ' + name + '! Ты справился за ' + counter + ' попытки!')
+
+if guess_number != number:
+    number = str(number)
+    print('Увы, ты не угадал, я загадал число ' + number)

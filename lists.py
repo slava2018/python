aeab9f22bd1963c0244c6b5833197307bd0adabe
slava <12345>
2020-03-04 19:39:44 +0300

    04032020

--- lists.py
+++ lists.py
@@ -1,25 +1,75 @@
 from lib import check_input
 from random import shuffle
+from time import sleep
 
 
 cards = [i for i in range(6,12)]*4
-points = 0
 shuffle(cards)
+
+player_points = 0
+player_cards = 0
 answer = 'да'
+
+print('Ход игрока\n')
 
 while answer == 'да':
     # перетаскивает карту в руку
     handler = cards.pop()
-    points += handler
-    if points > 21:
+    player_cards +=1
+    player_points += handler
+    if player_points > 21:
         print('Перебор')
+        player_points = 0
         break
-    elif points == 21:
-        print('***21***')
+    elif player_points == 21:
+        print('21!')
+        break
     else:
         answer = check_input(input(f'''
-        Вам выпало: {handler}.
-        Очки: {points}.
-        Еще? (да/нет)\n
+    Вам выпало: {handler}.
+    Очки: {player_points}.
+    Еще?\n
     '''), ['да', 'нет'], True)
 
+if player_points == 0:
+    print(f'Человек перебрал')
+else:
+    print(f'Очки человека: {player_points}')
+
+print('Ход компьютера\n')
+
+bot_points = 0
+answer = 'да'
+while answer == 'да':
+    # перетаскивает карту в руку
+    handler = cards.pop()
+    bot_points += handler
+    if bot_points > 21:
+        print('Перебор')
+        bot_points = 0
+        break
+    elif bot_points == 21:
+        print('21!')
+        break
+    else:
+        print('Еще?')
+        sleep(2)
+        if player_cards > 3:
+            answer = 'нет'
+        elif bot_points < 16:
+            answer = 'да'
+        else:
+            answer = 'нет'
+    print(answer)
+
+if bot_points == 0:
+    print(f'Компьютер перебрал')
+else:
+    print(f'Очки компьютера: {bot_points}\n')
+
+if bot_points == player_points:
+    print('Ничья!')
+elif bot_points < player_points:
+    print('Человек победил!')
+else:
+    print('Компьютер победил')

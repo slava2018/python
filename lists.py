11cad86754084a20817101cc8ea3aff664bec0a8
lyubomyr83 <lyubomyr83@mail.ru>
2020-03-10 18:00:14 +0300

    03032020

--- lists.py
+++ lists.py
@@ -2,76 +2,84 @@ from lib import check_input
 from random import shuffle, choice
 from time import sleep
 
-
-cards = [i for i in range(6,12)]*4
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10, 11] * 4
 shuffle(cards)
 
 player_points = 0
-player_cards = 0
+bot_points = 0
 answer = 'да'
-
-print('\nХод игрока\n')
+move = 1
 
 while answer == 'да':
     # перетаскивает карту в руку
-    handler = cards.pop()
-    player_cards +=1
-    player_points += handler
+    if move == 1:
+        dealer_handler = cards.pop()
+        bot_points += dealer_handler
+        sleep(2)
+        print(f'Компьютеру выпало: {dealer_handler}')
+
+        users_cards = []
+        for i in range(2):
+            handler = cards.pop()
+            users_cards.append(handler)
+            player_points += handler
+    else:
+        handler = cards.pop()
+        player_points += handler
+
     if player_points > 21:
+        print(f'Вам выпало: {handler}')
         print('Перебор')
         player_points = 0
         break
     elif player_points == 21:
-        print('21!')
+        print('Black Jack!!!')
         break
     else:
-        answer = check_input(input(f'''
+        if move == 1:
+            answer = check_input(input(f'''
+    =====================
+    ОЧКИ:
+    Оппонент: {bot_points}
+    Вы: {player_points}.
+    =====================
+    Вам выпало: {users_cards[1]} и {users_cards[0]}.
+    Еще?\n
+    '''), ['да', 'нет'], True)
+        else:
+            answer = check_input(input(f'''
+    =====================
+    ОЧКИ:
+    Оппонент: {bot_points}
+    Вы: {player_points}.
+    =====================
     Вам выпало: {handler}.
-    Очки: {player_points}.
     Еще?\n
     '''), ['да', 'нет'], True)
 
-if player_points == 0:
-    print(f'Человек перебрал')
+    move += 1
+
 else:
-    print(f'Очки человека: {player_points}')
-
-print('\nХод компьютера\n')
-
-bot_points = 0
-answer = 'да'
-while answer == 'да':
-    # перетаскивает карту в руку
-    handler = cards.pop()
-    bot_points += handler
-    if bot_points > 21:
-        print('Перебор')
-        bot_points = 0
-        break
-    elif bot_points == 21:
-        print('21!')
-        break
-    else:
-        print('Еще?')
+    while bot_points < 18:
+        dealer_handler = cards.pop()
+        bot_points += dealer_handler
         sleep(2)
-        if player_cards > 3:
-            answer = 'нет'
-        elif bot_points > 16:
-            answer = 'нет'
-        elif bot_points > 10:
-            answer = choice('да','нет')
-        else:
-            answer = 'да'
-    print(answer)
+        print(f'''
+        Компьютеру выпало: {dealer_handler}
+        =====================
+        ОЧКИ:
+        Оппонент: {bot_points}
+        Вы: {player_points}.
+        =====================
+            ''')
+        if bot_points > 21:
+            print('Перебор')
+            bot_points  = 0
 
-if bot_points == 0:
-    print(f'Компьютер перебрал')
-else:
-    print(f'Очки компьютера: {bot_points}\n')
 
 if bot_points == player_points:
     print('\nНичья!')
 elif bot_points < player_points:
-    print('\nЧеловек победил!')
+    print('\nТы победил!')
 else:
     print('\nКомпьютер победил')
